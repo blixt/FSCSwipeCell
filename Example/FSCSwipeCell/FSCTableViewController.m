2725cbfef56a9217154d727687a73ba70d4bb228
Blixt <me@blixt.org>
2015-02-16 17:10:43 -0500

    Update example to show a new view on left-to-right swipe

--- Example/FSCSwipeCell/FSCTableViewController.m
+++ Example/FSCSwipeCell/FSCTableViewController.m
@@ -3,6 +3,7 @@
 @interface FSCTableViewController ()
 
 @property (nonatomic, strong) NSMutableArray *labels;
+@property (nonatomic, weak) FSCSwipeCell *optionsCell;
 
 @end
 
@@ -18,44 +19,77 @@
 #pragma mark FSCSwipeCellDelegate
 
 - (void)swipeCell:(FSCSwipeCell *)cell didScroll:(CGFloat)distance side:(FSCSwipeCellSide)side {
-    cell.rightView.backgroundColor = [UIColor colorWithHue:0.2 saturation:1 brightness:MIN(0.5 + distance / 150, 1) alpha:1];
+    // Calculate a brightness based on how far the cell has been swiped (hits 1.0 when cell will open on its own when released).
+    CGFloat brightness = MIN(0.5 + distance / (kFSCSwipeCellOpenDistanceThreshold * 2), 1);
+
+    if (side == FSCSwipeCellSideLeft) {
+        cell.leftView.backgroundColor = [UIColor colorWithHue:0.8 saturation:1 brightness:brightness alpha:1];
+    } else if (side == FSCSwipeCellSideRight) {
+        cell.rightView.backgroundColor = [UIColor colorWithHue:0.2 saturation:1 brightness:brightness alpha:1];
+    }
 }
 
 - (void)swipeCellDidChangeCurrentSide:(FSCSwipeCell *)cell {
-    if (cell.currentSide == FSCSwipeCellSideRight) {
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Snooze"
-                                                                       message:@"How long do you want to snooze?"
-                                                                preferredStyle:UIAlertControllerStyleActionSheet];
+    switch (cell.currentSide) {
+        case FSCSwipeCellSideLeft:
+            // This is now the cell showing options.
+            self.optionsCell = cell;
+            break;
+        case FSCSwipeCellSideNone:
+            if (cell == self.optionsCell) {
+                // The cell is no longer open.
+                self.optionsCell = nil;
+            }
+            break;
+        case FSCSwipeCellSideRight:
+        {
+            // Show the snooze menu (all the code below but one line is for showing the iOS alert).
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Snooze"
+                                                                           message:@"How long do you want to snooze?"
+                                                                    preferredStyle:UIAlertControllerStyleActionSheet];
 
-        // This is only used by iPad.
-        UIPopoverPresentationController *popover = alert.popoverPresentationController;
-        if (popover) {
-            popover.sourceView = cell;
-            popover.sourceRect = cell.bounds;
-            popover.permittedArrowDirections = UIPopoverArrowDirectionAny;
+            // This is only used by iPad.
+            UIPopoverPresentationController *popover = alert.popoverPresentationController;
+            if (popover) {
+                popover.sourceView = cell;
+                popover.sourceRect = cell.bounds;
+                popover.permittedArrowDirections = UIPopoverArrowDirectionAny;
+            }
+
+            // This is the block that we will run if the user picks an option.
+            void (^remove)(UIAlertAction *) = ^void(UIAlertAction *action) {
+                // Don't do this at home, kids.
+                NSUInteger index = [self.labels indexOfObject:cell.textLabel.text];
+                // Remove the row from view.
+                [self.labels removeObjectAtIndex:index];
+                [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]]
+                                      withRowAnimation:UITableViewRowAnimationAutomatic];
+            };
+
+            [alert addAction:[UIAlertAction actionWithTitle:@"5 minutes" style:UIAlertActionStyleDefault handler:remove]];
+            [alert addAction:[UIAlertAction actionWithTitle:@"One hour" style:UIAlertActionStyleDefault handler:remove]];
+            [alert addAction:[UIAlertAction actionWithTitle:@"Until tomorrow" style:UIAlertActionStyleDefault handler:remove]];
+
+            // If the user cancels, simply reset the cell.
+            [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+                // This line is the only FSCSwipeCell-related code for snoozing. It will "close" the open cell.
+                cell.currentSide = FSCSwipeCellSideNone;
+            }]];
+
+            [self presentViewController:alert animated:YES completion:nil];
+            break;
         }
-
-        // This is the block that we will run if the user picks an option.
-        void (^remove)(UIAlertAction *) = ^void(UIAlertAction *action) {
-            // Don't do this at home, kids.
-            NSUInteger index = [self.labels indexOfObject:cell.textLabel.text];
-            // Remove the row from view.
-            [self.labels removeObjectAtIndex:index];
-            [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]]
-                                  withRowAnimation:UITableViewRowAnimationAutomatic];
-        };
-
-        [alert addAction:[UIAlertAction actionWithTitle:@"5 minutes" style:UIAlertActionStyleDefault handler:remove]];
-        [alert addAction:[UIAlertAction actionWithTitle:@"One hour" style:UIAlertActionStyleDefault handler:remove]];
-        [alert addAction:[UIAlertAction actionWithTitle:@"Until tomorrow" style:UIAlertActionStyleDefault handler:remove]];
-
-        // If the user cancels, simply reset the cell.
-        [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-            cell.currentSide = FSCSwipeCellSideNone;
-        }]];
-
-        [self presentViewController:alert animated:YES completion:nil];
     }
+}
+
+- (BOOL)swipeCell:(FSCSwipeCell *)cell shouldShowSide:(FSCSwipeCellSide)side {
+    if (side == FSCSwipeCellSideLeft && self.optionsCell && cell != self.optionsCell) {
+        // We only want to display one cell with options at the time. Close the other one first.
+        self.optionsCell.currentSide = FSCSwipeCellSideNone;
+        return NO;
+    }
+
+    return YES;
 }
 
 #pragma mark UITableViewDataSource
@@ -68,13 +102,16 @@
     if (!cell) {
         cell = [[FSCSwipeCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
         cell.delegate = self;
+
+        // Create the left view which will contain options.
+        cell.leftView = [[UIView alloc] init];
+
+        // Create the right view which will be our "Snooze" action.
+        cell.rightView = [[UIView alloc] init];
     }
 
-    // Create the right view which will be our "Snooze" action.
-    cell.rightView = [[UIView alloc] init];
-
     // Set up the labels.
-    cell.detailTextLabel.text = @"Swipe right to archive, left to snooze.";
+    cell.detailTextLabel.text = @"Swipe right for options, left to snooze.";
     cell.detailTextLabel.textColor = [UIColor grayColor];
     cell.textLabel.text = self.labels[indexPath.row];
 
